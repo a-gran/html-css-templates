7475c84c885b0a629da096b07c98a13bf074bd8b
Aristarh Gran <awebdeff@gmail.com>
2022-11-27 19:56:45 +0300

    add ibg/, modified img.html, img.css

---
+++ ibg/ibg.js
@@ -0,0 +1,22 @@
+// чистый js
+
+function ibg() {
+	let ibg = document.querySelectorAll(".ibg");
+	for (let i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+ibg();
+
+// jQuery
+
+function ibg() {
+	$.each($('.ibg'), function (index, val) {
+		if ($(this).find('img').length > 0) {
+			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+		}
+	});
+}
+ibg();
